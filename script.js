f588296ba9fb11fab97223211201e89ebb7337f8
remyrachelamold <remyrachelamold@gmail.com>
2026-03-14 00:49:43 +0530

    Initial commit with portfolio website files

---
+++ script.js
@@ -0,0 +1,23 @@
+new Typed("#typing",{
+
+strings:[
+"Software Engineer",
+"AI Developer",
+"Full Stack Developer"
+],
+
+typeSpeed:60,
+backSpeed:30,
+loop:true
+
+});
+
+
+ScrollReveal().reveal('.reveal',{
+
+distance:'40px',
+duration:1000,
+origin:'bottom',
+interval:200
+
+});
